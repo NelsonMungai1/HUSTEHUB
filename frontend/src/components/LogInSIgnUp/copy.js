15a8e6817c3605dc98937310b25043741dee90bf
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-27 16:11:35 +0300

    Delete copy file

--- frontend/src/components/LogInSIgnUp/copy.js
+++
@@ -1,8 +0,0 @@
-{action === "Sign Up" && ( // Conditionally render password confirmation field
-                    <div className='input'>
-                        <img src={password_icon} alt="/" />
-                        <input type="password" placeholder='Confirm Password' onChange={(e) => setPasswordConfirm(e.target.value)} />
-                    </div>
-                )}
-
-                const [passwordConfirm, setPasswordConfirm] = useState(""); // Added state for password confirmation
