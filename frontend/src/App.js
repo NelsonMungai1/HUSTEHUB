c282dce2c27b4f332d45a3d982c9313a144c64c5
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-27 15:10:41 +0300

    Handle login and redirect url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import LogInSignUp from './components/LogInSIgnUp/LogInSignUp';
 
+// import cors from 'cors';
+
+// const app = express();
+// App.use(cors());
+
 function App() {
   return (
     <div>
--- frontend/src/components/LogInSIgnUp/LogInSignUp.js
+++ frontend/src/components/LogInSIgnUp/LogInSignUp.js
@@ -8,18 +8,26 @@ import password_icon from '../Assets/password.png';
 
 const LogInSignUp = () => {
     const [action, setAction] = useState("Sign Up");
-    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const handleSignUp = async () => {
         try {
             const response = await axios.post('http://127.0.0.1:8000/api/v1/accounts/registration/', {
-                name,
+                username,
                 email,
                 password,
             });
+            // if (response.status === 200) {
+            //     history.push('/');
+            // }
+            if (response.status === 200) {
+                window.location.href = 'https://www.google.com';
+              }
+              
             console.log(response.data); // handle success response
+            console.log(`Username: ${username}`); // handle success response
         } catch (error) {
             console.error(error); // handle error
         }
@@ -28,9 +36,15 @@ const LogInSignUp = () => {
     const handleLogin = async () => {
         try {
             const response = await axios.post('http://127.0.0.1:8000/api/v1/accounts/login/', {
+                username,
                 email,
                 password,
             });
+            // if (response.status == T)
+            if (response.status === 200) {
+                window.location.href = 'https://www.google.com';
+            }
+
             console.log(response.data); // handle success response
         } catch (error) {
             console.error(error); // handle error
@@ -46,8 +60,12 @@ const LogInSignUp = () => {
             <div className='inputs'>
                 {action === "Login" ? null : <div className='input'>
                     <img src={user_icon} alt="/" />
-                    <input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)} />
+                    <input type="text" placeholder='Name' onChange={(e) => setUsername(e.target.value)} />
                 </div>}
+                <div className='input'>
+                    <img src={user_icon} alt="/" />
+                    <input type="text" placeholder='Name' onChange={(e) => setUsername(e.target.value)} />
+                </div>
                 <div className='input'>
                     <img src={email_icon} alt="/" />
                     <input type="email" placeholder='Email Id' onChange={(e) => setEmail(e.target.value)} />
@@ -56,13 +74,15 @@ const LogInSignUp = () => {
                     <img src={password_icon} alt="/" />
                     <input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)} />
                 </div>
+                
             </div>
             {action === "Sign Up" ? null : <div className="forgot-password">Forgot password?<span>Click here</span></div>}
 
             <div className="submit-container">
                 {/* dynamic assignment of class names */}
-                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { handleSignUp(); setAction("Sign Up") }}>Sign Up</div>
-                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { handleLogin(); setAction("Login") }}>Log In</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { handleSignUp(); setAction("Sign Up") }}>Sign Up</div>
+                
+                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { handleLogin(); setAction("Login") }}>Log In</div>
             </div>
         </div>
     );
